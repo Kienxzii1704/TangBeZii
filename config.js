8e1eb6497e3e0988c8884e508f96bf558fd61b20
Kienxzii1704 <88337934+Kienxzii1704@users.noreply.github.com>
2021-08-04 09:54:53 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bé Ziii oiii !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào     tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Bấm vào đây tiếp nhóo',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là iuuuu anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: '',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Bé có gì muốn nói thì nhắn tin cho anh nhóooo',
+    btnAccept: 'Dạ, vaanggggggg ạ',
+    messLink: 'https://www.facebook.com/messages/t/100045534649094' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
